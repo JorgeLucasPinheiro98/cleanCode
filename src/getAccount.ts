dc124a8d32a914925676ce3d50127e140e2caade
Rodrigo Branas <rodrigo.branas@gmail.com>
2025-02-11 08:37:38 -0300

    updated

---
+++ src/getAccount.ts
@@ -0,0 +1,14 @@
+import AccountDAO from "./data";
+
+export default class GetAccount {
+
+	constructor (readonly accountDAO: AccountDAO) {
+	}
+
+	async execute (accountId: string) {
+		const output = await this.accountDAO.getAccountById(accountId);
+		return output;
+	}
+}
+
+
